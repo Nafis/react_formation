ce4364793cfd17b274e8355748c9d30fea73f95b
nafis.fa3 <nafis.fa3@gmail.com>
2025-10-08 12:39:09 +0100

    templated added

---
+++ mynotes.md
@@ -0,0 +1,5 @@
+start yarn globally:
+npm install -g yarn ==> globally dans ton pc
+yarn dev
+npx generate-react-cli ==> for authomatiser les tree folders
+npx generate-react-cli c TemplateName ==> c for component
---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css'
+
+const Button = () => {
+  return ( 
+  <div className="Button" data-testid="Button">
+    Button Component
+  </div>
+  )
+};
+
+
+export default Button;
